34d55c882d373eaff7bf4056f89a30f21d43e5d1
tpolonen <tpolonen@student.hive.fi>
2025-10-11 09:40:36 +0300

    Fix path structure

---
+++ parse.c
@@ -0,0 +1,148 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "parse.h"
+
+#define MAX_EMPLOYEE_COUNT 123456 //arbitrary for now
+#define FAIL 1
+#define OK 0
+
+void* xmalloc(size_t size)
+{
+	if (size == 0)
+	{
+		return NULL;
+	}
+	//tag memory for freeing on exit?
+	return malloc(size);
+}
+
+int check_header(struct dbheader_t* headerPtr, unsigned int filesize)
+{
+	if (headerPtr->magic != HEADER_MAGIC ||
+		headerPtr->version != DB_VERSION ||
+		headerPtr->count > MAX_EMPLOYEE_COUNT ||
+		headerPtr->filesize != filesize)
+	{
+		//ERRNO not set
+		return FAIL;
+	}
+	return OK;
+}
+
+int create_db_header(struct dbheader_t **headerOut)
+{
+	if (headerOut == NULL)
+	{
+		return FAIL;
+	}
+
+	*headerOut = (struct dbheader_t *)xmalloc(sizeof(struct dbheader_t));
+	if (*headerOut == NULL)
+	{
+		return FAIL;
+	}
+
+	(*headerOut)->magic = HEADER_MAGIC;
+	(*headerOut)->version = 1;
+	(*headerOut)->count = 0;
+	(*headerOut)->filesize = 0;
+	return OK;
+}
+
+int validate_db_header(int fd, struct dbheader_t **headerOut)
+{
+	if (headerOut == NULL)
+	{
+		//ERRNO not set
+		return FAIL;
+	}
+	
+	size_t headerSize = sizeof(struct dbheader_t);
+	char headerBuf[headerSize];
+	if (read(fd, headerBuf, headerSize) < headerSize)
+	{
+		return FAIL;
+	}
+
+	struct stat fileStat = {0};
+	if (fstat(fd, &fileStat) < 0)
+	{
+		return FAIL;
+	}
+
+	struct dbheader_t* headerPtr = (struct dbheader_t*)headerBuf;
+	if (check_header(headerPtr, (unsigned int)fileStat.st_size != OK))
+	{
+		//ERRNO not set
+		return FAIL;
+	}
+
+	*headerOut = (struct dbheader_t *)xmalloc(headerSize);
+	if (*headerOut == NULL)
+	{
+		return FAIL;
+	}
+
+	bcopy(headerPtr, *headerOut, headerSize);
+	return OK;
+}
+
+int read_employees(int fd, struct dbheader_t *headerIn, struct employee_t **employeesOut)
+{
+	if (employeesOut == NULL)
+	{
+		//ERRNO not set
+		return FAIL;
+	}
+
+	//so. this looks a bit funky.
+	//basically we just want to check we're not fed some random junk data
+	//outside of normal control flow.
+	//magic is most likely to fail in junk data scenario,
+	//it's good to verify count anyway before mallocing,
+	//filesize shouldn't matter at this point since we are not(?) dealing
+	//with the file itself anymore.
+	if (check_header(headerIn, headerIn->filesize))
+	{
+		//ERRNO not set
+		return FAIL;
+	}
+	
+	size_t employeesSize = sizeof(struct employee_t) * headerIn->count;
+	*employeesOut = (struct employee_t *)xmalloc(employeesSize);
+	if (*employeesOut == NULL)
+	{
+		return FAIL;
+	}
+	
+	if (read(fd, *employeesOut, employeesSize) < employeesSize)
+	{
+		free(*employeesOut);
+		return FAIL;
+	}
+	return OK;
+}
+
+int output_file(int fd, struct dbheader_t *headerIn, struct employee_t *employees)
+{
+	if (check_header(headerIn, headerIn->filesize))
+	{
+		//ERRNO not set
+		return FAIL;
+	}
+	
+	if (write(fd, headerIn, sizeof(struct dbheader_t)) < sizeof(struct dbheader_t))
+	{
+		return FAIL;
+	}
+	if (write(fd, employees, sizeof(struct employee_t) * headerIn->count) <
+			sizeof(struct employee_t) * headerIn->count)
+	{
+		return FAIL;
+	}
+	return OK;
+}
---
+++ parse.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#define HEADER_MAGIC 0x4c4c4144
+#define NAME_LEN 256
+#define ADDRESS_LEN 256
+#define DB_VERSION 1
+
+struct dbheader_t {
+        unsigned int magic;
+        unsigned short version;
+        unsigned short count;
+        unsigned int filesize;
+};
+
+struct employee_t {
+        char name[NAME_LEN];
+        char address[ADDRESS_LEN];
+        unsigned int hours;
+};
+
+int create_db_header(struct dbheader_t **headerOut);
+int validate_db_header(int fd, struct dbheader_t **headerOut);
+int read_employees(int fd, struct dbheader_t * headerIn, struct employee_t **employeesOut);
+int output_file(int fd, struct dbheader_t * headerIn, struct employee_t *employees);
